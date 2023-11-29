04d7d4a79bbabaebbfa27a82d7eca591e866e2e7
Kevin Baluyot <baluyotkevin33@yahoo.com>
2023-11-28 16:42:40 -0800

    created schema and added mongoosedb

--- lib/scraper/index.ts
+++ lib/scraper/index.ts
@@ -71,9 +71,12 @@ export async function scrapeAmazonProduct(url: string) {
             stars: 4.5,
             isOutOfStock: outOfStock,
             description,
+            lowestPrice: Number(currentPrice) || Number(originalPrice),
+            highestPrice: Number(originalPrice) || Number(currentPrice),
+            averagePrice: Number(currentPrice) || Number(originalPrice)
         }
 
-
+        return data;
     } catch (err: any) {
         throw new Error(`Failed to scrape product: ${err.message}`)
     }
